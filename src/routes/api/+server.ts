6fc19022dc4ed2a20f443a00d05accf615f0bbe5
Alexander Demin <ademin@gmail.com>
2023-06-12 17:09:27 +0100

    Experiments with error

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,18 +1,20 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
-export async function POST({ request}) {
+export async function POST({ request }) {
     const data = await request.json();
-    let query = {};
+    let query: { [key:string]:string }  = {};
     const entries = new URL(request.url).searchParams.entries();
     for (let [key, value] of entries) {
         query[key] = value;
-        if (key == 'boom') { return json({ error: 'boom' }, { status: 400 }); }
+        if (key == "boom") {
+            throw error(400, "!!!");
+        }
     }
     return json({
         request: data,
         query: query,
         url: request.url,
-        env: process.env
+        env: process.env,
     });
 }
